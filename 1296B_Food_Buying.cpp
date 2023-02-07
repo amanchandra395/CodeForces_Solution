ff89f893c8186d757e357933c0e6ee10621efb77
Aman Chandra <112195895+amanchandra395@users.noreply.github.com>
2023-02-08 00:22:23 +0530

    Math

---
+++ 1296B_Food_Buying.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    long long int n,ans,div;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        ans=n,div=0;
+        if(n<10){
+            cout<<n<<"\n";
+            continue;
+        }
+
+        else{
+            while(n>=10){
+                div=n/10;
+                n=n%10;
+                n=n+div;
+                ans+=div;
+            }
+            cout<<ans<<"\n";
+        }
+    }
+    return 0;
+}
