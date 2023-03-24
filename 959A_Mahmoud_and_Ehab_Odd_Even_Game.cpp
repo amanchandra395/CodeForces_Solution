dc30c4d576100f51dd4a8649def284d81c53d0fe
Aman Chandra <112195895+amanchandra395@users.noreply.github.com>
2023-03-24 11:43:24 +0530

    Implementation

---
+++ 959A_Mahmoud_and_Ehab_Odd_Even_Game.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    if(n==0)
+    {
+        cout<<"Ehab"<<endl;
+    }
+    else if(n%2==1)
+    {
+        cout<<"Ehab"<<endl;
+    }
+    else
+    {
+        cout<<"Mahmoud"<<endl;
+    }
+    return 0;
+}
